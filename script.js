f800a01a403b0929f765a3d68bcd46501f70319e
OCG <40301816+ornitcg@users.noreply.github.com>
2023-10-16 01:02:41 +0300

    improvements on gallery

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-if (2+3 === 5) {
-    alert("you are smart");
-}
+const folderUrl = 'C:\Users\tinko\MyRepos\sheshVeargamanWebsite\images\ourBrides';
+
+// Function to fetch the list of files in the folder
+// function fetchFiles() {
+//     try {
+        
+alert(document.getElementsByClassName("lace"));
